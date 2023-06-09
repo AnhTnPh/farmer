b5c407bc7054d9fd541d731f63c5e0f78c6cda79
AnhTnPh <anhtn@infoplusvn.com>
2023-06-09 15:59:11 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/landingpage-farmer/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
 })
